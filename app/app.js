dc4fa92392e589b29124f231bf18caa6f0f005d2
JohnVanClouds <sephiroh@gmail.com>
2018-12-06 22:03:03 +0100

    improve list of stations and move stations list to outside configuration file

--- app/app.js
+++ app/app.js
@@ -1,141 +1,145 @@
-// remote: ip:52222 
+// remote: 192.168.1.38:52222 
+var ws = null;
+var stations = {};
+var playlist = [];
+var audio;
+var rdv = null;
 
-console.log("script loaded!!");
-
-var radios;
 var activRadio = null;
-var sellId;
-var player;
-var isPlaying = false;
+var sellId = -1;
 
-window.addEventListener("load", function() {
-  radios = [];
-  createPlayer();
-  createGrid();
+window.addEventListener("load", function () {
+	//console.log("init");
+
+	rdv = document.getElementById("radio-lists");
+
+	audio = document.createElement("audio"); // that needs insert to html document or not plays :/
+	audio.autoplay = true;
+	audio.volume = 0.20;
+  
+  	document.body.appendChild(audio);
+
+	reconnect();
 });
 
-window.addEventListener("keyup", function(e){
-  switch(e.keyCode)
-  {
-    case 13: //ok
-      radios[sellId].click();
-      break;
-    case 38: //up
-      if(radios[sellId]){
-        radios[sellId].className = "radio"; 
-        if(--sellId < 0){sellId = 0;}
-        radios[sellId].className = "radio selected";
-      }
-      break;
-    case 40: //down
-      if(radios[sellId]){
-        radios[sellId].className = "radio"; 
-        if(++sellId >= radios.length){sellId = radios.length-1;}
-        radios[sellId].className = "radio selected";
-      }
-      break;
-  }
+window.addEventListener("keyup", function (e) {
+	switch (e.keyCode) {
+		case 13: //ok
+			playlist[sellId].click();
+			break;
+		case 38: //up
+			if (playlist[sellId]) {
+				playlist[sellId].className = "radio";
+				if (--sellId < 0) { sellId = 0; }
+				playlist[sellId].className = "radio selected";
+			}
+			break;
+		case 40: //down
+			if (playlist[sellId]) {
+				playlist[sellId].className = "radio";
+				if (++sellId >= playlist.length) { sellId = playlist.length - 1; }
+				playlist[sellId].className = "radio selected";
+			}
+			break;
+	}
 });
 
-function createGrid(){
-  var radio_list = [
-    {
-      name: "Vox Fm",
-      img: "voxfm.png",
-      src: "http://waw03.ic1.scdn.smcloud.net/t049-1.mp3"
-    },
-    {
-      name: "Rmf Fm",
-      img: "rmffm.png",
-      src: "http://31.192.216.10/RMFFM48"
-    },
-    {
-      name: "Radio Złote Przeboje",
-      img: "zloteprzeboje.png",
-      src: "http://poznan5-6.radio.pionier.net.pl:8000/tuba9-1.mp3"
-    },
-    {
-      name: "Radio Eska",
-      img: "eska.png",
-      src: "http://publish.acdn.smcloud.net:8000/t042-1.mp3"
-    },
-    {
-      name: "Antyradio",
-      img: "antyradio.png",
-      src: "http://dcs-188-64-85-35.atmcdn.pl/sc/o2/Eurozet/live/antyradio.livx?audio=5/;stream.nsv?seed=119.60711795836687"
-    }
-  ];
-  
-  var rdv = document.getElementById("radio-lists");
-  
-  for (id in radio_list){
-    var rd = radio_list[id]; // aktualnei przetwarzane radio
-    
-    var div = createElement("div", {"className": "radio", "radioId": id, "streamSource": rd.src})
-
-    var play  = createElement("div", {"className": "play"});
-    
-    var logo  = createElement("div", {"className": "logo"});
-    logo.style.backgroundImage = "url(img/" + rd.img+")";
-    
-    var title = createElement("div", {"className": "title", "innerHTML": rd.name});
-    
-    div.appendChild(play);
-    div.appendChild(logo);
-    div.appendChild(title);
-
-    div.addEventListener("click", onClick);
-    
-    radios.push(div);
-    rdv.appendChild(div); // dom el with radio list
-  }
-  
-  if(radios.length > 0)
-  {
-    sellId = 0;
-    radios[0].id = "active";
-    radios[sellId].className = "radio selected";
-    radios[sellId].click();
-  }
+function reconnect() {
+	//console.log("reconnect");
+	
+	ws = new WebSocket("ws://188.165.224.82:9002");
+	ws.onmessage = function (e) { parsePlaylist(JSON.parse(e.data)) };
+	ws.onclose = function () { setTimeout(reconnect(), 10 * 1000); };
 }
 
-function createPlayer(){
-  console.log("player created!");
-  
-  player = document.createElement("audio");
-  player.autoplay = false;
-  player.volume = 0.25;
-  
-  document.body.appendChild(player);
+function parsePlaylist(data) {
+	//console.log("config parsing");
+	
+	var new_stations = {};
+	var new_playlist = [];
+	var old_sell_id = sellId;
+	var old_station_name = "";
+	var found = false;
+
+	for (var n in data.stations) {
+		var st =  data.stations[n]
+
+		var div = createElement("div", { "className": "radio", "radioId": n, "radioName": st.name, "streamSource": st.stream });
+		new_stations[st.name] = div;
+		new_playlist.push(div);
+
+		if (sellId >= 0 && !found && st.name === playlist[sellId]) {
+			console.log("found id: " + n + " old id: " + sellId);
+			sellId = n;
+			found = true;
+		}
+		
+		if (stations[st.name])
+			delete stations[st.name];
+
+		var play = createElement("div", { "className": "play" });
+		var title = createElement("div", { "className": "title", "innerHTML": st.name });
+
+		div.appendChild(play);
+		div.appendChild(title);
+
+		div.addEventListener("click", onClick);
+	}
+
+	rdv.innerHTML = "";
+	for (var n in new_playlist) {
+		rdv.appendChild(new_playlist[n]);
+	}
+
+	if (!found) {
+		sellId = 0;
+	} else {
+		console.log("setting active element after load config");
+		new_playlist[sellId].id = "active";
+	}
+
+	new_playlist[0].className = "radio selected";
+
+	playlist = new_playlist;
+	stations = new_stations;
 }
 
-function onClick(){
-  console.log("radio clicked!");
-  
-  radios[sellId].className = "radio"; 
-  radios[sellId].className = "radio selected";
-  
-  var active = document.getElementById("active");
-  active.id = undefined;
-  activRadio = this;
-  this.id = "active";
+function playStation(div) {
+	//console.log("play station!", div);
+	
+	audio.pause();
+	//audio.currentTime = 0;
+	audio.src = "";
 
-  if(this.radioId != sellId){
-    player.pause();
-  }
-  player.src = activRadio.streamSource;
-  player.play();
+	//console.log("start playing: " + div.streamSource);
+	audio.src = div.streamSource; //.replace(".aac", ".mp3");
+	audio.play();
 }
 
-function createElement(type, options){
-  if(!type) return;
-  var el = document.createElement(type);
-  
-  if(options && typeof options === "object"){
-    for(id in options){
-      el[id] = options[id];
-    }
-  }
-  
-  return el;
+function onClick() {
+	//console.log("cliecked radio" + this.radioId + " => " + this.streamSource);
+
+	playlist[sellId].className = "radio";
+	playlist[sellId].className = "radio selected";
+
+	var active = document.getElementById("active");
+	if(active) active.id = undefined;
+	this.id = "active";
+
+	sellId = this.radioId;
+
+	playStation(this);
+}
+
+function createElement(type, options) {
+	if (!type) return;
+	var el = document.createElement(type);
+
+	if (options && typeof options === "object") {
+		for (id in options) {
+			el[id] = options[id];
+		}
+	}
+
+	return el;
 }
